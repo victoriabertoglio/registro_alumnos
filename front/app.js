37a2ba8758727725dbfc1459af1897d6ff6e4751
victoriabertoglio <victoriabertoglio6@gmail.com>
2025-06-23 19:29:56 -0300

    Comentario de prueba

--- front/app.js
+++ front/app.js
@@ -1,6 +1,6 @@
 const API_URL = "http://localhost:5001/api/students";
 const API_KEY = "12345ABCDEF";
-//Comentario de prueba
+//Comentario prueba
 // Headers comunes para todas las peticiones
 const headers = {
     "Content-Type": "application/json",
